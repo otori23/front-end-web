f839eea90bd14fed7f4bb7ece8c24bd6dfd850a4
otori23 <azeez.otori@gmail.com>
2015-07-14 14:13:52 -0700

    implement cat clicker requirements v2

--- 13_JavaScript_Design_Patterns/Cat Clicker Requirements/js/script.js
+++ 13_JavaScript_Design_Patterns/Cat Clicker Requirements/js/script.js
@@ -1,12 +1,19 @@
-var catClickCount = 0;
-var catClickCountElem;
-var catClickEventHander = function() {
-	if(catClickCountElem === undefined) {
-		catClickCountElem = document.getElementById('count-item');
-	}
-	catClickCount++;
-	catClickCountElem.textContent = catClickCount;
+var catClickEventHander = function(e) {
+	var catImg = document.getElementById(e.srcElement.id);
+	var catClickCountElem = catImg.previousElementSibling;
+	var catClickCount = parseInt(catClickCountElem.textContent);
+	catClickCountElem.textContent = ++catClickCount;
 };
 
+
+var catName1 = "cat1";
+var catName2 = "cat2";
+
+document.getElementById('cat-name1').textContent = catName1;
+document.getElementById('cat-name2').textContent = catName2;
+
 var catImage = document.getElementById('cat1-image');
+catImage.addEventListener("click", catClickEventHander, false);
+
+catImage = document.getElementById('cat2-image');
 catImage.addEventListener("click", catClickEventHander, false);
